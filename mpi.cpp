c008905d676e05a50b1abaee7e83c5011e261f06
naaatami <144642305+naaatami@users.noreply.github.com>
2025-04-10 20:15:00 -0500

    broken but more complete mpi

--- mpi.cpp
+++ mpi.cpp
@@ -18,6 +18,12 @@ using namespace std;
 const int maxIterations = 100;
 const int limit = 4;
 
+struct Color {
+    double h;
+    double s;
+    double v;
+};
+
 // these values are all set a bit weird to crop better and add some margins - parts are cut off if you round these off to "nicer" numbers
 double xMin = -2.1;
 double xMax = 0.6;
@@ -25,8 +31,8 @@ double yMin = -1.2;
 double yMax = 1.2;
 
 double calculateMandelbrot(complex<double> c);
-vector<double> findColor(double iterationCount);
-
+Color findColor(double iterationCount);
+MPI_Datatype create_mpi_color_type();
 
 int main(int argc, char *argv[])
 {
@@ -38,9 +44,10 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(mainComm, &rank);
     int width, height;
     string filename;
-    vector<vector<tuple<double>>> colorVector;
+    vector<vector<Color>> colorArray;
 
     CImg<float> mandelbrotImage;
+
 
     if(rank == 0)
     {
@@ -53,15 +60,14 @@ int main(int argc, char *argv[])
         width = atoi(argv[1]);
         height = atoi(argv[2]);
         filename = argv[3];
+        vector<vector<Color>> colorArray(width, vector<Color>(height));
 
         // width and height are obvious
         // 1 represents the depth (image dimension across z index, so obviously just one)
         // 3 is the color spectrum - RGB coded in this case
         // 0 floods the whole initial image with black (not sure this is true xd)
         mandelbrotImage(width, height, 1, 3, 0);
-        colorVector = vector<vector<vector<double>>>(width, vector<vector<double>>(height, tuple<double>(3)));
     }
-
 
     double startTime, elapsedTime;
     MPI_Barrier(mainComm);
@@ -70,24 +76,42 @@ int main(int argc, char *argv[])
     MPI_Bcast(&width, 1, MPI_INT, 0, mainComm);
     MPI_Bcast(&height, 1, MPI_INT, 0, mainComm);
 
+    int localHeight = height/numberOfProcessors; //range of workelapsedTime
+    int startHeight = rank * localHeight;    //where it starts
+    int endHeight = localHeight * (rank+1); //where it ends
+
+    Color localColorArray[width][localHeight];
     for(int x = 0; x < width; x++)
     {
-        for(int y = 0; y < height; y++)
+        for(int y = startHeight; y < endHeight; y++)
         {
+            // in localcolorarray, it needs to save starting at 0, but the iterating still has to be done from startHeight to endHeight...
             double xScaled = xMin + x*(xMax - xMin) / width;
-            double yScaled = yMin + y*(yMax - yMin) / height;
+            double yScaled = yMin + y*(yMax - yMin) / localHeight;
 
             complex<double> c(xScaled, yScaled);
             double iterationCount = calculateMandelbrot(c);
-            vector<double> color = findColor(iterationCount);
-            mandelbrotImage.draw_point(x, y, color.data());
+            localColorArray[x][y] = findColor(iterationCount);
+
         }
     }
+
+
+    MPI_Datatype MPI_COLOR_TYPE = create_mpi_color_type();
+    MPI_Gather(localColorArray, width * localHeight, MPI_COLOR_TYPE, &colorArray, width * height, MPI_COLOR_TYPE, 0, mainComm);
 
     elapsedTime = MPI_Wtime() - startTime;
 
     if(rank == 0)
     {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = startHeight; y < endHeight; y++)
+            {
+                mandelbrotImage.draw_point(x, y, vector<double>{colorArray[x][y].h, colorArray[x][y].s, colorArray[x][y].v}.data());
+            }
+        }
+
         mandelbrotImage.HSVtoRGB().save_png(filename.c_str());
         cout << "Total time to find: " << elapsedTime;
     }
@@ -100,6 +124,7 @@ int main(int argc, char *argv[])
 // note that this returns a double, not an int! it's normalizing the iterationcount, which makes it a double
 double calculateMandelbrot(complex<double> c)
 {
+
     int currentIterations = 0;
     complex<double> z(0.0, 0.0);
 
@@ -128,18 +153,38 @@ double calculateMandelbrot(complex<double> c)
 // we want to have fun, so we change that into a percent so we can then change it to the range 200 - 360 (which makes it blue -> pink)
 // then we do smoothPercent because otherwise, we have a VERY strong change from blue to pink. this smooths it out
 
-vector<double> findColor(double iterationCount)
+Color findColor(double iterationCount)
 {
     double baseColor = 360.0 * iterationCount / maxIterations;
     double baseColorPercent = (baseColor - 0.0) / (360.0 - 0.0);
     double smoothPercent = pow(baseColorPercent, 0.32);
     double scaledColor = ((360 - 200) * smoothPercent) + 200;
 
-    vector<double> color  = {
+    Color color = {
         scaledColor,
         1.0, //max saturation
         (iterationCount < maxIterations) ? 1.0 : 0 //if part of set, make black
     };
 
     return color;
+}
+
+MPI_Datatype create_mpi_color_type(){
+
+    MPI_Datatype MPI_COLOR_TYPE;
+
+    int count = 3;
+    int block_lengths[3] = {1, 1, 1};
+    MPI_Datatype old_types[3] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
+
+    MPI_Aint indices[3];
+    indices[0] = (MPI_Aint) offsetof(struct Color, h);
+    indices[1] = (MPI_Aint) offsetof(struct Color, s);
+    indices[2] = (MPI_Aint) offsetof(struct Color, v);
+
+
+    MPI_Type_create_struct(count, block_lengths, indices, old_types, &MPI_COLOR_TYPE);
+    MPI_Type_commit(&MPI_COLOR_TYPE);
+
+    return MPI_COLOR_TYPE;
 }
